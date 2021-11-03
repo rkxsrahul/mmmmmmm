55c497f603652eaaffa83e9868924fd390e0ffc3
Rahul Kumar <xs109-rahkum@xs-host603-041.xs.neuralcompany.team>
2021-11-03 17:08:41 +0530

    ahcfhg

---
+++ src/nats/method.go
@@ -0,0 +1,37 @@
+package nats
+
+import (
+	"encoding/json"
+	"log"
+	"strings"
+)
+
+func findCategory(mapd map[string]string) string {
+	for _, v := range mapd {
+		return v
+	}
+	return ""
+}
+
+func headerFileSlug(header string) string {
+	header = strings.ToLower(header)
+	header = strings.Replace(header, " ", "-", -1)
+	header = strings.Replace(header, "/", "-", -1)
+	return header + ".sh"
+}
+
+type BodyData struct {
+	Method string `json:"method"`
+	UUID   string `json:"uuid"`
+	URL    string `json:"url"`
+	Status string `json:"status"`
+}
+
+func convertDatatoJSON(bdata []byte) BodyData {
+	var data BodyData
+	err := json.Unmarshal(bdata, &data)
+	if err != nil {
+		log.Println(err)
+	}
+	return data
+}
---
+++ src/nats/publish.go
@@ -0,0 +1,21 @@
+package nats
+
+import (
+	"log"
+
+	"git.xenonstack.com/akirastack/continuous-security-deployments/config"
+)
+
+type RequestData struct {
+	Data   map[string]interface{} `json:"data"`
+	UUID   string                 `json:"uuid"`
+	Method string                 `json:"method"`
+	Header string                 `json:"header"`
+}
+
+func Publish(data []byte, subject string) {
+	log.Println(string(data), subject)
+	if err := config.NC.Publish("scan-results", data); err != nil {
+		log.Fatal(err)
+	}
+}
